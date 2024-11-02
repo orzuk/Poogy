3a70127cd96e88979f0edd5f07217d265303073a
orzuk <or.zuk@mail.huji.ac.il>
2024-11-02 16:10:21 +0200

    Added binary tree splot algorithm

--- phylo_utils.py
+++ phylo_utils.py
@@ -6,7 +6,9 @@ import numpy as np
 import os
 import copy
 import importlib
-from Bio import AlignIO, Phylo
+from Bio import Phylo
+import pickle
+import sys
 
 
 def split_tree_at_root(tree):
@@ -20,8 +22,8 @@ def split_tree_at_root(tree):
     # Create two subtrees based on the root's children
     subtree_1, subtree_2 = children
 
-    # Rename the internal nodes of the new subtrees
-    subtree_1.name, subtree_2.name = "Subtree_1", "Subtree_2"
+    # Rename the internal nodes of the new subtrees. Cancel!!
+#    subtree_1.name, subtree_2.name = "Subtree_1", "Subtree_2"
 
     # Create two separate trees with each subtree
     tree_1 = Phylo.BaseTree.Tree(root=subtree_1)
@@ -59,9 +61,6 @@ def extract_and_prune_model(input_file, species_to_remove, output_file):
 
     if not tree.rooted:
         tree.root_with_outgroup(tree.root)
-    print("THIS IS GOOD??")
-    print(tree)
-    print(species_to_remove)
     pruned_tree = prune_and_update_tree(tree, species_to_remove)
 
     # Step 3: Write the pruned tree back into the original model
@@ -163,7 +162,11 @@ def get_induced_subtree(tree, root_name):
     # Find the specified node in the tree
     root_clade = subtree.find_any(name=root_name)
     if root_clade is None:
-        raise ValueError(f"No clade with the name '{root_name}' found in the tree.")
+        leaves = root_name.split("-")
+        if len(leaves) == 2:
+            root_clade = subtree.find_any(name=tree.common_ancestor(leaves).name)
+        if root_clade is None:
+            raise ValueError(f"No clade with the name '{root_name}' found in the tree.")
 
     # Trim the tree down to the specified root node and its descendants
     subtree.root = root_clade
@@ -171,7 +174,6 @@ def get_induced_subtree(tree, root_name):
 
     # Detach all nodes above the specified root to make it the new root
     subtree.collapse_all(lambda clade: clade is not root_clade)
-
     return subtree
 
 
@@ -188,8 +190,8 @@ def get_complementary_tree(tree, sub_tree):
     """
     if isinstance(sub_tree, str):
         sub_tree = get_induced_subtree(tree, sub_tree)
-    print("sub_tree is: ", sub_tree)
-    print("terminals are: ", sub_tree.get_terminals())
+    print("sub_tree is: ", sub_tree.root.name)
+#    print("terminals are: ", sub_tree.get_terminals())
     # Get the names of all species (terminal nodes) in sub_tree
     sub_tree_species = {leaf.name for leaf in sub_tree.get_terminals()}
 
@@ -283,9 +285,9 @@ def read_phylop_output(output_file, plot_flag = False):
                 continue
 #
 #            # Split the line into components (assumes format: chr, pos, score)
-            print("line is: ", line)
+#            print("line is: ", line)
             cur_line = line.strip().split()
-            print(cur_line)
+#            print(cur_line)
             chrom, position, score = line.strip().split()
 #
 #            # Convert position and score to appropriate types
@@ -328,8 +330,7 @@ def run_phylop_linux(tree_file, msa_file, output_file, sub_tree="", method="SCOR
     command = f"/mnt/c/Code/Github/phast/bin/phyloP --method {method} --subtree {sub_tree} --mode {mode} --base-by-base " + pos_str + \
               f" {tree_file}  {msa_file} > {output_file}"
 
-    print("Run phylop command: ")
-    print(command)
+    print("Run phylop command: ", command)
     # Call the WSL command from Windows
     subprocess.run(command, shell=True)  # now wsl !!!
 #    with open(output_file, "w") as outfile:
@@ -343,7 +344,12 @@ def run_phylop_linux(tree_file, msa_file, output_file, sub_tree="", method="SCOR
 
 
 # New: split tree recursively and output subtree with different rates !
-def fit_tree_rates(tree_file, msa_file, output_file, fdr_alpha = 0.1,  method="SCORE", mode="CONACC", plot_tree=False):
+def fit_tree_rates(tree_file, msa_file, output_file, fdr_alpha = 0.1,  phylop_score="SCORE", mode="CONACC", method="brute-force", plot_tree=False):
+    return 0
+
+
+# New: split tree into two subtree with different rates and output trees and rates
+def fit_two_subtree_rates(tree_file, msa_file, output_file, phylop_score="SCORE", mode="CONACC", method="brute-force", plot_tree=False):
     tree = Phylo.read(tree_file, "newick")
     root = tree.root
     children = root.clades  # Get the two child clades (subtrees)
@@ -351,25 +357,43 @@ def fit_tree_rates(tree_file, msa_file, output_file, fdr_alpha = 0.1,  method="S
 
     # Decide if significant or not
     output_dir = os.path.dirname(output_file)
-    best_subtree, best_score = find_best_rate_split_in_tree(tree_file, msa_file, output_dir, "greedy")
-    print("Best Subtree: ", best_subtree)
-    print(type(best_subtree))
+    best_subtree, best_score = find_best_rate_split_in_tree(tree_file, msa_file, output_dir, method)
+    print("Best Subtree: ", best_subtree if isinstance(best_subtree, str) else best_subtree.root.name)
     print("Best score: ", best_score)
     comp_best_tree = get_complementary_tree(tree, best_subtree) # Fit rates for the two trees
     if plot_tree:
-        print("Plotting! Saving in: ", output_file.replace(".out", ".png"))
-        subtree_rates = subtrees_to_color_vector(tree, [get_induced_subtree(tree, best_subtree)])  # Compute rates and color
-        color_tree(tree, values=subtree_rates, cmap_name='coolwarm', output_file=output_file.replace(".out", ".png"))
+        output_plot_tree_file = output_file.replace(".out", "_" + method + ".png")
+        print("Plotting! Saving in: ", output_plot_tree_file)
+#        with open('tree_with_sub.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+#            pickle.dump([tree, best_subtree], f)
+
+    #    print("Inducded subtree=", get_induced_subtree(tree, best_subtree))
+#        print("Rates=?")
+        if isinstance(best_subtree, str):  # convert to tree object
+            best_subtree = get_induced_subtree(tree, best_subtree)
+        print("Best subtree=", best_subtree.root.name)
+        subtree_rates = subtrees_to_color_vector(tree, [best_subtree])  # Compute rates and color
+#        print(subtree_rates)
+        # Saving the objects:
+        with open('tree_to_plot.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+            pickle.dump([tree, subtree_rates, output_plot_tree_file], f)
+
+        color_tree(tree, values=subtree_rates, cmap_name='coolwarm', output_file=output_plot_tree_file)
 
     return best_subtree, best_score
 
 
 # Split the tree such that the contrast in rate between the two halves is maximal.
-# Default: Use greedy approach with binary search. Alternative is to enumerate all possible splits
+# Default: Use greedy approach with binary search. Alternative is to enumerate all possible splits with brute-force
 def find_best_rate_split_in_tree(tree_file, msa_file, output_dir, method="binary"):
     tree = Phylo.read(tree_file,  "newick")
     print("Start best rate split, output-dir=", output_dir)
-    if method == "greedy":  # enumerate over all possible splits
+
+    # Aggregate single base scores into one summary score (e.g. average)
+    def aggregate_scores(phylop_scores):
+        return sum([x['teststat'] for x in phylop_scores])
+
+    if method == "brute-force":  # enumerate over all possible splits
         best_subtree = None
         best_score = float('-inf')
 
@@ -383,7 +407,7 @@ def find_best_rate_split_in_tree(tree_file, msa_file, output_dir, method="binary
                 continue
 
             # Create a temporary output file for PhyloP results
-            print("output dir: ", output_dir)
+#            print("output dir: ", output_dir)
             output_file = os.path.join(output_dir, f"phylop_{subtree_name}.out")
 
             print("Run phylop outputfile=", output_file)
@@ -392,7 +416,9 @@ def find_best_rate_split_in_tree(tree_file, msa_file, output_dir, method="binary
                                              sub_tree=subtree_name, mode="CONACC", method="SCORE", read_output=True)
             print("Finished phylop outputfile=", output_file)
             # Process the PhyloP output and calculate the score difference
-            cur_score = sum([ x['teststat'] for x in phylop_scores])
+            cur_score = aggregate_scores(phylop_scores)
+            print("cur_score=", cur_score)
+            print("best_score=", best_score)
 
             # Track the subtree/species with the maximum score
             if cur_score > best_score:
@@ -404,18 +430,49 @@ def find_best_rate_split_in_tree(tree_file, msa_file, output_dir, method="binary
         return best_subtree, best_score
 
     if method == "binary":  # split tree binary each time
-        best_subtree = None
-        best_score = float('-inf')
-        clade = tree.root  # split at the root
+        sub_trees = split_tree_at_root(tree)
+
+        for side in range(2):  # try splitting both sides
+            output_file = os.path.join(output_dir, f"phylop_{sub_trees[side].root.name}.out")
+            phylop_scores = run_phylop_linux(tree_file, msa_file, output_file, \
+                    sub_tree=sub_trees[side].root.name, mode="CONACC", method="SCORE", read_output=True)
+            best_score = aggregate_scores(phylop_scores)
+            best_subtree = sub_trees[side]
+            print("best_score=", best_score)
+            print("best_score_type=", type(best_score))
+            print("phylop_score_type=", type(phylop_scores), " ; len=", len(phylop_scores))
+
+            if isinstance(best_score, list):
+                print("WTF? ")
+                sys.exit("Wrong format for best score!! ")
 
 
-        subtree_name = clade.name or f"clade_{clade.confidence}"  # Use name or confidence if unnamed
 
+            # Now run recursively:
+            found_better_score = True
+            while best_subtree.count_terminals() > 1 and found_better_score:  # Recursive splitting
+                found_better_score = False
+                cur_sub_trees = split_tree_at_root(best_subtree)
+                for t in range(2):  # loop on two subtrees
+                    cur_phylop_scores = run_phylop_linux(tree_file, msa_file, output_file, \
+                                                         sub_tree=cur_sub_trees[t].root.name, mode="CONACC", method="SCORE", read_output=True)
+                    cur_score = aggregate_scores(cur_phylop_scores)
+                    print("cur_score=", cur_score)
+#                    print("best_score_still=", best_score)
+                    print("cur_score, best_score_types=", type(cur_score), type(best_score))
+                    print("cur_phylop_score, best_phylop_score_types=", type(cur_phylop_scores), type(phylop_scores))
 
-        phylop_scores = run_phylop_linux(tree_file, msa_file, output_file, \
-                sub_tree=subtree_name, mode="CONACC", method="SCORE", read_output=True)
+                    if cur_score > best_score:  # choose the best
+                        print("Found better!!")
+                        best_score = cur_score
+                        best_subtree = cur_sub_trees[t]
+                        found_better_score = True
 
-
-        # Run on two sub-trees:
-        
-
+            if side == 0:
+                side_best_score = best_score
+                side_best_subtree = best_subtree
+            else:  # last side, compare and return
+                if best_score > side_best_score:
+                    return best_subtree, best_score
+                else:
+                    return side_best_subtree, side_best_score
--- run_phylop.py
+++ run_phylop.py
@@ -10,16 +10,36 @@ from phylo_utils import *
 from phylo_plot_utils import *
 from msa_utils import *
 from sys import platform
+import argparse
+
+
+# Create an argument parser object
+parser = argparse.ArgumentParser(description="Run tree rate splitting with optional arguments.")
+
+# Add arguments with their default values
+parser.add_argument('--alignment', type=str, default='multiz470', help='Path to the tree file')
+parser.add_argument('--k', type=int, default=10, help='Kmer length')
+parser.add_argument('--run_mode', type=str, default='plot_trees', help='Mode to run the script')  # Here decide what to do
+parser.add_argument('--pos', type=str, default='', help='Genomic coordinates')  # Here decide what to do
+parser.add_argument('--verbose', action='store_true', help='Enable verbose mode')  # Example for boolean operation
+
+
+# Parse the command-line arguments
+args = parser.parse_args()
 
 # Operations to do:
 reload_modules = False
-plot_trees = False
-prune_tree_by_msa_species = False
-run_phylop = False
-run_phylop_recursive = False
-parse_msa = True
-alignment = "multiz470" # "multiz470"  # multiz30
+plot_trees = args.run_mode == "plot_trees"
+prune_tree_by_msa_species = args.run_mode == "prune_tree_by_msa"
+run_phylop = args.run_mode == "run_phylop"
+run_phylop_best_split = args.run_mode == "run_phylop_best_split"
+run_phylop_recursive = args.run_mode == "plot_phylop_recursive"
+parse_msa = args.run_mode == "parse_msa"
+help = args.run_mode == "help"
+alignment = args.alignment # "multiz470" # "multiz470"  # multiz30
 
+if help:
+    print("Usage: python3 run_phylop --alignemt=<alignment-file>, --k=<kmer-length> --run_mode=<operation-to-run>")
 
 if reload_modules:
     import phylo_utils
@@ -44,12 +64,10 @@ elif platform == "win32" or platform == "win64":
     run_os = "windows" # Windows...
     g_dir = "G://My Drive/"
 
-
-
 # Set main data directory:
 data_dir = g_dir + "Data/hg38_470Vert"
 
-if alignment == "multiz470": # Run entire algorithm: determine the best tree division for acceleration and conservation for a segment
+if alignment == "multiz30":  # Run entire algorithm: determine the best tree division for acceleration and conservation for a segment
     tree_file = data_dir + "/phylop470/hg38.phyloP470way.mod"
     msa_file = data_dir + "/multiz470/PAR_CHR8_5217000_5217500.maf"  # why one block?
     phylop_str = "phylop470"
@@ -58,9 +76,16 @@ else:
     tree_file = data_dir + "/phylop30/hg38.phyloP30way_named.mod"
     msa_file = data_dir + "/multiz30/chr22_GL383583v2_alt.maf"
     phylop_str = "phylop30"
-    start_pos, end_pos = None, None  # do not extract sub-alignment
+    start_pos, end_pos = 47, 82  # do not extract sub-alignment  # 12-mer
+if args.pos != "":  # extract positions from input (override defaults)
+    start_pos, end_pos = [int(s) for s in args.pos.split("-")]
 
-element_str = os.path.basename(msa_file)[:-4]
+
+if start_pos is None:
+    pos_str = ""
+else:
+    pos_str = "_" + str(start_pos) + "_" + str(end_pos)
+element_str = os.path.basename(msa_file)[:-4] + pos_str
 output_dir = data_dir + "/output"
 sub_tree = "calJac3-saiBol1"
 output_phylop_file = data_dir + "/output/phylop/phylop_from_python_subtree_" + sub_tree + ".out"
@@ -104,7 +129,7 @@ if plot_trees:
     color_tree(tree, values=values_dict, output_file=data_dir + "/output/trees/output_tree_hg38_randcolors.png")
 
 
-if run_phylop:
+if run_phylop:  # just run on a simple split of the tree into two
     # With unix !!! # wsl
     phylop_command = (
         "/mnt/c/Code/Github/phast/bin/phyloP --method SCORE "  
@@ -124,8 +149,8 @@ if run_phylop:
     print("ran using run_phylop_linux!!!")
 
 
-if run_phylop_recursive:
-    print("Running tree partitioning!!!")
+if run_phylop_best_split:  # find automatically the best split into two complementary sub-trees
+    print("Running tree partitioning to two sub-trees!!!")
     # Example usage
 #    output_file = "/mnt/g/My Drive/Data/hg38_470Vert/output/phylop/phylop_from_python_subtree_calJac3-saiBol1.out"
 #    phylop_data = read_phylop_output_fixed_step(output_phylop_file, plot_flag=True)
@@ -138,34 +163,49 @@ if run_phylop_recursive:
     # read alignment:
     msa_blocks = AlignIO.parse(msa_file, "maf")
     msa_species = set()
+    start_pos_vec, end_pos_vec = [], []
     for block in msa_blocks:
         for record in block:
             # Extract the species name (assuming it's the prefix before a dot)
             species = record.id.split('.')[0]
             msa_species.add(species)
 
+            start_pos_vec.append(record.annotations["start"])
+            end_pos_vec.append(start_pos_vec[-1] + record.annotations["size"])
+
+#    print("start: ", start_pos_vec)
+#    print("end: ", end_pos_vec)
+
     # Extract species names in the tree
     tree_species = {leaf.name for leaf in tree.get_terminals()}
 
     # Find species missing in the MSA
     missing_species_in_msa = tree_species - msa_species
-#    print("pruning:")
-#    print(tree)
-#    print(missing_species_in_msa)
-#    if not tree.rooted:
-#        tree.root_with_outgroup(tree.root)
-#    pruned_tree = prune_and_update_tree(tree, missing_species_in_msa)
-#    print("pruned!!")
     pruned_tree_file = tree_file[:-4] + "_pruned.mod"
     extract_and_prune_model(tree_file, missing_species_in_msa, pruned_tree_file)
 
     print("Find best rate split!!!")
-    best_subtree, best_score = fit_tree_rates(pruned_tree_file, msa_file, output_dir + "/" + phylop_str + "/" + element_str + "_split_tree.out", plot_tree=True)
-    print("Found best rate split: ", best_subtree, best_score)
+#    best_subtree, best_score = fit_two_subtree_rates(pruned_tree_file, msa_file, output_dir + "/" + phylop_str + "/" + element_str + "_split_tree.out", plot_tree=True)
+#    print("Found best rate split: ", best_subtree, best_score)
+    best_subtree_binary, best_score_binary = fit_two_subtree_rates(pruned_tree_file, msa_file, output_dir + "/" + phylop_str + "/" + element_str + "_split_tree.out", plot_tree=True, method="binary")
+    print("Found best rate split: ", best_subtree_binary.root.name, best_score_binary)
+
+    # Getting back the objects:
+#with open('tree_to_plot.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+#        tree, subtree_rates, output_plot_tree_file = pickle.load(f)
+with open('tree_with_sub.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+    tree, best_subtree = pickle.load(f)
 
 
-if parse_msa:
-    tree30 = Phylo.read(data_dir + "/phylop30/hg38.phyloP30way_named.mod", "newick") # TEMP HARD CODED 30
+
+# split recursively into multiple subtrees with different rates
+if run_phylop_recursive:
+    print("Run recursive!!! ")
+    # T.B.D.
+
+
+if parse_msa:  # read and extract sub-alignment
+    tree30 = Phylo.read(data_dir + "/phylop30/hg38.phyloP30way_named.mod", "newick") # TEMP HARD CODED 30WAY !!
     msa_output_file = msa_file[:-4] + "_pos_" + str(start_pos) + "_" + str(end_pos) + ".maf"
     msa_selected_blocks = extract_subalignment(msa_file, start_pos, end_pos, msa_output_file)
     msa_selected_blocks2 = AlignIO.parse(msa_file, "maf")
